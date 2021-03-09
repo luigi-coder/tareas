37286ed6e02d5cc3edc23e5f02a8796ee0d0eb14
Luis <luis@iMac-Salinas-Invernizzi.local>
2021-03-09 18:36:22 -0300

    Segundo compromiso

--- js/javascript.js
+++ js/javascript.js
@@ -5,21 +5,21 @@ var despertador = function(hr) {
     if(hr <= 5) {
         return `<h1><span>${nombre}</span> tu descanso aun no termina, segui en la cama porfavor </h1>`
     }else if(hr >= 6 && hr <=9) {
-        return `${nombre} buenos dias, ya podes despertar y empezar con tu primera actividad del dia ENTRENAR`
+        return `<h1><span>${nombre}</span> buenos dias, ya podes despertar y empezar con tu primera actividad del dia ENTRENAR</h1>`
     }else if (hr >= 9 && hr <=10){
-        return `${nombre} es hora de desayunar,`
+        return `<h1><span>${nombre}</span> es hora de desayunar</h1>`
     }else if (hr >= 10 && hr <= 13) {
-        return `${nombre}, estas en horario de trabajo, no podes moverte de tu lugar`
+        return `<h1><span>${nombre}</span> estas en horario de trabajo, no podes moverte de tu lugar</h1>`
     }else if (hr >= 13 && hr <= 14) {
-        return `${nombre}, hora de almorzar`
+        return `<h1><span>${nombre}</span> hora de almorzar</h1>`
     }else if (hr >= 14 && hr <= 18) {
-        return `${nombre} relajate es hora de estar con Emma 17`
+        return `<h1><span>${nombre}</span> relajate es hora de estar con Emma 17</h1>`
     }else if (hr >= 19 && hr <= 21) {
-        return `${nombre}, es un buen momento para cenar`
+        return `<h1><span>${nombre}</span> es un buen momento para cenar</h1>`
     }else if (hr >= 21 && hr <= 23) {
-        return `${nombre}, mirate una serie`
+        return `<h1><span>${nombre}</span> mirate una serie</h1>`
     }else {
-        return `Por que carajos estas despierta, si es hora de dormir`
+        return `<h1><span>Por que carajos estas despierta, si es hora de dormir</h1>`
     }
 }
 document.write(despertador(hora))
